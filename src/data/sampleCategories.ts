e3715bc2f54c4af610bd800c826705a6fd4f2ce9
Abdullah <abdullah.atkaev@gmail.com>
2025-08-12 13:29:43 +0300

    Update sampleCategories.ts

--- src/data/sampleCategories.ts
+++ src/data/sampleCategories.ts
@@ -3,7 +3,5 @@ import { sampleCourse } from './sampleCourse';
 
 export const sampleCategories: Category[] = [
   { id: 'strength', title: 'Силовые тренировки', courses: [sampleCourse] },
-  { id: 'posture', title: 'Осанка', courses: [] },
-  { id: 'recovery', title: 'Восстановление после операции', courses: [] },
-  { id: 'acl', title: 'Реабилитация после ПКС', courses: [] },
+  { id: 'posture', title: 'Тренировки на выносливость', courses: [] }
 ];
