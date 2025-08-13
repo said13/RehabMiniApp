4c4f1252288370813e4bec4089c5e49dd0469577
Abdullah <abdullah.atkaev@gmail.com>
2025-08-13 11:31:47 +0300

    feat: add migration helper

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const { createServer } = require('http');
 const next = require('next');
+const { runMigrations } = require('./src/db/migrate');
 
 const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    handle(req, res);
-  }).listen(port, () => {
-    console.log(`> Server running on http://localhost:${port}`);
+runMigrations()
+  .catch((err) => {
+    console.error('Database migration failed', err);
+    process.exit(1);
+  })
+  .then(() => {
+    app.prepare().then(() => {
+      createServer((req, res) => {
+        handle(req, res);
+      }).listen(port, () => {
+        console.log(`> Server running on http://localhost:${port}`);
+      });
+    });
   });
-});
