a2313c064f4f1ffb524c5611d1e617a26f4e6862
Abdullah <abdullah.atkaev@gmail.com>
2025-08-13 15:33:47 +0300

    Use defineConfig for drizzle and set DATABASE_URL

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,10 @@
-import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
-export default {
+export default defineConfig({
   schema: "./src/db/schema.ts",
   out: "./drizzle",
-  driver: "pg",
   dialect: "postgresql",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
   },
-} satisfies Config;
+});
