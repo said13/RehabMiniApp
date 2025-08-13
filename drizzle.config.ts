8766a5bbd2f268296c41dd10f39b1f4f53c0270b
Abdullah <abdullah.atkaev@gmail.com>
2025-08-13 15:04:44 +0300

    Add PostgreSQL dialect to Drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,6 +4,7 @@ export default {
   schema: "./src/db/schema.ts",
   out: "./drizzle",
   driver: "pg",
+  dialect: "postgresql",
   dbCredentials: {
     connectionString: process.env.DATABASE_URL!,
   },
